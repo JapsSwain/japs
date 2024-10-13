117a23cf59b2dd739fd2fab9cc603be890a33750
JAPS <108985566+JapsSwain@users.noreply.github.com>
2024-10-14 01:32:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+/*-------sticky-----*/
+
+/*----hehe---*/
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
+
+
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100; 
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height) {
+
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+      }
+
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+  }
+
+
+  
+
+   
+  
+/*-------about-----*/
+var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+          for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+          }
+          for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+          }
+
+          event.currentTarget.classList.add("active-link");
+          document.getElementById(tabname).classList.add("active-tab");
+        }
+
+/*-------about-----*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+        
+menuIcon.addEventListener('click',() => {
+  menuIcon.classList.toggle('fa-x');
+  navbar.classList.toggle('active');
+})
